e2a20cc5fe303f078623ab0a9edf26ec03b3053f
karasevmain <karasevmain@gmail.com>
2024-08-21 20:30:50 +0300

    Initializing the Project

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
